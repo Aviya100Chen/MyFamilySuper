abdb1d14520eaa1939723f3165ad97532e677557
Talmid <aviyachen10@gmail.com>
2024-12-02 09:35:01 +0200

    עדיין יש בעיה בתצוגה

---
+++ app/src/main/java/com/example/myfamilyssuper/sign_up_screen.java
@@ -0,0 +1,30 @@
+package com.example.myfamilyssuper;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class sign_up_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up_screen);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // כאן נעשה המעבר Activity במקום בתוך הListener
+        Intent intent = new Intent(sign_up_screen.this, LogInScreen.class);
+        startActivity(intent);
+    }
+}
