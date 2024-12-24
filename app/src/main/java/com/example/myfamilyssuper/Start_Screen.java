7dc86c971c3137b8d95c613d1fe9f0c8608e0544
Teacher <aviyachen10@gmail.com>
2024-12-24 10:49:39 +0200

    עשיתי מעבר אינטנט

--- app/src/main/java/com/example/myfamilyssuper/Start_Screen.java
+++ app/src/main/java/com/example/myfamilyssuper/Start_Screen.java
@@ -1,6 +1,9 @@
 package com.example.myfamilyssuper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,30 @@ public class Start_Screen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button productsButton = findViewById(R.id.product_button);
+
+        productsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // מעבר למסך בשם products
+                Intent intent = new Intent(Start_Screen.this, products.class);
+                startActivity(intent);
+            }
+        });
+        Button myCart_buttonButton = findViewById(R.id.myCart_button);
+
+        myCart_buttonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // מעבר למסך בשם myCart
+                Intent intent = new Intent(Start_Screen.this, MyCart.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
