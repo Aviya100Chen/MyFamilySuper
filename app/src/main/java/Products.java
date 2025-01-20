53cf5e7aa92e7916b129fcd83d8bf917c36b26a0
Teacher <aviyachen10@gmail.com>
2025-01-20 09:46:23 +0200

    התחלתי לכתוב את הCLASS של המוצרים. צריך להמשיך לעשות את הליסט ווי. ולתקן קצת את המסכים האחרים+ לעשות להם אינטנט.

---
+++ app/src/main/java/Products.java
@@ -0,0 +1,42 @@
+public class Products {
+    private String category;
+    private String image;
+    private String name;
+    private int price;
+
+    public Products(String category, String image, String name, int price){
+        this.category=category;
+        this.image=image;
+        this.name=name;
+        this.price=price;
+    }
+
+    public Products(){
+
+    }
+    public String getCategory(){
+        return category;
+    }
+    public String getImage(){
+        return image;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getPrice(){
+        return price;
+    }
+
+    public void setCategory(String category){
+        this.category=category;
+    }
+    public void setImage(String image){
+        this.image=image;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setPrice(int price){
+        this.price=price;
+    }
+}
